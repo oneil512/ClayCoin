935a69abf1f821c25943f0913b11b19d81e2ff47
clay.o'neil <Clay.O'Neil@bestbuy.com>
2018-07-31 14:32:37 -0500

    fill out main

--- src/main/java/com.clay/Main.java
+++ src/main/java/com.clay/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
     public static void main(String[] args){
 
+	Wallet wallet = new Wallet();
+	Blockchain blockchain = new Blockchain();
+
+	Node node = new Node(blockchain, wallet);
+
+	wallet.sendTransaction(wallet.getAddress(), wallet.getAddress());
+
+	node.mine(4);
 
 
     }
